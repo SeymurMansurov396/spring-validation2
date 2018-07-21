7ecfa278ef1522bb90ea6c1a86f6aabdf2f36de6
Seymur Mansurov <seymur168@gmail.com>
2018-07-21 23:36:48 +0400

    elave

--- src/main/java/com/seymur/model/Customer.java
+++ src/main/java/com/seymur/model/Customer.java
@@ -75,4 +75,24 @@ public class Customer {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
